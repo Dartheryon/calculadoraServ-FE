3dfec7a1bdf2acbe5d2f92e3e34732dd40396315
William <dandelius@gmail.com>
2024-03-15 18:39:07 -0500

    Added Toast at the moment of copying the text

--- src/components/DataPresentation.tsx
+++ src/components/DataPresentation.tsx
@@ -1,4 +1,5 @@
 import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { Toaster, toast } from 'react-hot-toast'
 interface Props {
   isMessageReady: boolean
   message: string
@@ -10,16 +11,17 @@ const DataPresentation = ({
   return isMessageReady && (
     <div className='flex flex-col'>
       <h2 className='text-center text-xl mx-5 my-3 text-black'>Toca el siguiente texto para copiarlo y poder enviarlo a los destinatarios</h2>
+      <Toaster position="bottom-center" />
       <div
         className=" bg-white shadow-md px-5 pt-6 pb-8 rounded-2xl my-3 mx-5"
       >
-        <span className='font-sans'>
-          <CopyToClipboard text={message}>
+        <CopyToClipboard text={message}>
+          <span onClick={() => toast.success('Texto Copiado al portapapeles')} className='font-sans'>
             <pre className='whitespace-pre-wrap w-full'>
               {message}
             </pre>
-          </CopyToClipboard>
-        </span>
+          </span>
+        </CopyToClipboard>
       </div>
     </div>
   )
