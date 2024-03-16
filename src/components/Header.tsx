539f608930427811ae611963203ee588aa5fd74c
William <dandelius@gmail.com>
2024-03-15 20:24:57 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <>
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Calculadora servicios {''}
-        <span className="text-indigo-600">Suba</span></h1>
+        <span className="text-emerald-600">Suba</span></h1>
     </>
   )
 }
